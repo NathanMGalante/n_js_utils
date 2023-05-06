a79b53ec212053d1a614c8c2d92d6f246efcd0e4
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2023-05-06 15:05:11 -0300

    Update completer-util.js

--- completer-util.js
+++ completer-util.js
@@ -1,9 +1,10 @@
-let completerMap = {};
-
-let anyCompleterKeyNumber = 0;
-
 /**
- * @param {String} key The key
+ * Creates a completer object with methods for completing and rejecting a Promise, as well as the Promise itself.
+ * If a key is provided, it is used as a unique identifier for the completer object, otherwise a default key is used.
+ * The completer object is stored in an internal map with the provided key as the key-value pair, which can later be retrieved using the `getCompleter` function.
+ *
+ * @param {string|null} key - A unique identifier for the completer object (optional).
+ * @returns {object} An object with methods for completing and rejecting a Promise, as well as the Promise itself.
  */
 export const completer = (key = null) => {
   let complete;
@@ -29,8 +30,11 @@ export const completer = (key = null) => {
 };
 
 /**
- * @param {String} key [Required] The key
+ * Retrieves a completer object from the internal map using the provided key.
+ *
+ * @param {string} key - The unique identifier of the completer object.
+ * @returns {object|null} The completer object associated with the provided key, or null if no object was found.
  */
 export const getCompleter = (key) => {
-  return completerMap[key];
+  return completerMap[key] || null;
 };
