ed7b2f4723dfdfb2408d13720318a65e9235ebbf
NathanMGalante <76235377+NathanMGalante@users.noreply.github.com>
2023-05-06 16:02:59 -0300

    Update delay-util.js

--- delay-util.js
+++ delay-util.js
@@ -1,7 +1,13 @@
+/**
+ * Returns a Promise that resolves after a specified duration.
+ *
+ * @param {number} [duration=100] - The duration to delay in milliseconds. Defaults to 100ms if not specified.
+ * @returns {Promise} A Promise that resolves after the specified duration.
+ */
 export const delay = (duration = 100) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("");
+      resolve();
     }, duration);
   });
 };
